f623750ef29279fc5838c04b0e2eb7acc85d70b4
OJ Raqueño <oj.raqueno@gmail.com>
2017-11-17 10:33:20 +0800

    some logging updates

--- JPRSC.HRIS/JPRSC.HRIS.WebApp/Infrastructure/Security/AuthorizePermissionAttribute.cs
+++ JPRSC.HRIS/JPRSC.HRIS.WebApp/Infrastructure/Security/AuthorizePermissionAttribute.cs
@@ -6,36 +6,52 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JPRSC.HRIS.Infrastructure.Logging;
+using System.Net;
+using JPRSC.HRIS.WebApp.Infrastructure.Logging;
 
 namespace JPRSC.HRIS.WebApp.Infrastructure.Security
 {
     public class AuthorizePermissionAttribute : AuthorizeAttribute
     {
-        private readonly ApplicationDbContext _db;
-
-        public AuthorizePermissionAttribute()
+        public AuthorizePermissionAttribute(Permission permission)
         {
-            _db = DependencyConfig.Instance.Container.GetInstance<ApplicationDbContext>();
+            Permission = permission;
         }
 
-        public Permission Permission { get; set; }
+        public Permission Permission { get; private set; }
 
         protected override bool AuthorizeCore(HttpContextBase httpContext)
         {
             var isAuthorized = base.AuthorizeCore(httpContext);
             if (!isAuthorized) return false;
 
+            var db = DependencyConfig.Instance.Container.GetInstance<ApplicationDbContext>();
+
             var currentUserId = httpContext.User.Identity.GetUserId();
 
-            var currentUser = _db.Users
+            var currentUser = db.Users
                 .Include(u => u.CustomRoles)
                 .SingleOrDefault(u => u.Id == currentUserId);
 
-            return currentUser
+            var authorizedViaPermission = currentUser
                 .CustomRoles
-                .SelectMany(r => r.Permissions)
-                .Distinct()
-                .Any(p => p == Permission);
+                .Any(cr => cr.HasPermission(Permission));
+
+            if (!authorizedViaPermission)
+            {
+                var logEntry = LogHelper.CreateLogEntryFromCurrentContext(LogLevel.Warn, "Unauthorized Request");
+                Logger.Log(logEntry);
+            }
+
+            return authorizedViaPermission;
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.HttpContext.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+            filterContext.HttpContext.Response.End();
+            filterContext.HttpContext.Response.Close();
         }
     }
 }
