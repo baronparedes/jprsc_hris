afd888ccdce9b681e817391efaaeb890b6e9c343
OJ Raqueño <oj.raqueno@gmail.com>
2018-08-16 11:04:29 +0800

    fix logic of determining whether a loan should be included in the payroll process

--- JPRSC.HRIS/JPRSC.HRIS.WebApp/Features/Payroll/Process.cs
+++ JPRSC.HRIS/JPRSC.HRIS.WebApp/Features/Payroll/Process.cs
@@ -97,7 +97,7 @@ namespace JPRSC.HRIS.WebApp.Features.Payroll
                     .ToListAsync();
 
                 var loans = await _db.Loans
-                    .Where(l => !l.DeletedOn.HasValue && clientEmployeeIds.Contains(l.EmployeeId.Value) && l.RemainingBalance > 0 && !l.ZeroedOutOn.HasValue && DbFunctions.TruncateTime(l.StartDeductionDate) >= DbFunctions.TruncateTime(command.PayrollPeriodFrom))
+                    .Where(l => !l.DeletedOn.HasValue && clientEmployeeIds.Contains(l.EmployeeId.Value) && l.RemainingBalance > 0 && !l.ZeroedOutOn.HasValue && DbFunctions.TruncateTime(l.StartDeductionDate) <= DbFunctions.TruncateTime(command.PayrollPeriodFrom))
                     .ToListAsync();
 
                 loans = loans.Where(l => l.LoanPayrollPeriods.Contains(command.PayrollPeriod.Value)).ToList();
