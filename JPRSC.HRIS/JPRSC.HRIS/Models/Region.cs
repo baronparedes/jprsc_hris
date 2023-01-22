7ada639b333dc37dd84aec367e90d198c8f0724a
Macky Dangalan <mackydangalan@gmail.com>
2023-01-22 17:23:20 +0800

    *Fixed headers of Alphalist reports. *Changed labeling of regions when used in reports.

--- JPRSC.HRIS/JPRSC.HRIS/Models/Region.cs
+++ JPRSC.HRIS/JPRSC.HRIS/Models/Region.cs
@@ -4,55 +4,55 @@ namespace JPRSC.HRIS.Models
 {
     public enum Region
     {
-        [Display(Name = "NCR (National Capital Region)")]
+        [Display(Name = "NCR")]
         NCR = 10,
 
-        [Display(Name = "ARMM (Autonomous Region in Muslim Mindanao)")]
+        [Display(Name = "ARMM")]
         ARMM = 20,
 
-        [Display(Name = "CAR (Cordillera Administrative Region)")]
+        [Display(Name = "CAR")]
         CAR = 30,
 
-        [Display(Name = "Region 1 (Ilocos Region)")]
+        [Display(Name = "I")]
         Region1 = 40,
 
-        [Display(Name = "Region 2 (Cagayan Valley)")]
+        [Display(Name = "II")]
         Region2 = 50,
 
-        [Display(Name = "Region 3 (Central Luzon)")]
+        [Display(Name = "III")]
         Region3 = 60,
 
-        [Display(Name = "Region 4A (CALABARZON)")]
+        [Display(Name = "IV-A")]
         Region4A = 70,
 
-        [Display(Name = "Region 4B (MIMAROPA)")]
+        [Display(Name = "IV-B")]
         Region4B = 80,
 
-        [Display(Name = "Region 5 (Bicol Region)")]
+        [Display(Name = "V")]
         Region5 = 90,
 
-        [Display(Name = "Region 6 (Western Visayas)")]
+        [Display(Name = "VI")]
         Region6 = 100,
 
-        [Display(Name = "Region 7 (Central Visayas)")]
+        [Display(Name = "VII")]
         Region7 = 110,
 
-        [Display(Name = "Region 8 (Eastern Visayas)")]
+        [Display(Name = "VIII")]
         Region8 = 120,
 
-        [Display(Name = "Region 9 (Zamboanga Peninsula)")]
+        [Display(Name = "IX")]
         Region9 = 130,
 
-        [Display(Name = "Region 10 (Northern Mindanao)")]
+        [Display(Name = "X")]
         Region10 = 140,
 
-        [Display(Name = "Region 11 (Davao Region)")]
+        [Display(Name = "XI")]
         Region11 = 150,
 
-        [Display(Name = "Region 12 (SOCCSKSARGEN)")]
+        [Display(Name = "XII")]
         Region12 = 160,
 
-        [Display(Name = "Region 13 (Caraga Region)")]
+        [Display(Name = "XIII")]
         Region13 = 170
     }
 }
